3ce949fd61eecb6d8e399e2b1f4ae8cfdfd5502c
Acer <Acer@LAPTOP-RCV4E5SL>
2022-08-01 23:41:02 +0700

    First Commit

---
+++ Day1-Git and GitHub/src/com/capgemini/github/EclipseWithGitHub.java
@@ -0,0 +1,11 @@
+package com.capgemini.github;
+
+public class EclipseWithGitHub {
+
+	public static void main(String[] args) {
+		// Print the line
+		System.out.println("Welcome to C2TC Program");
+		
+	}
+
+}
